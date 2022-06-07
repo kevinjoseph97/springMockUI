792e4445ae68aee3afdd8c4cb15538dd568d6401
kevinjoseph97 <kevinjoseph97@gmail.com>
2022-06-07 16:16:47 -0400

    implements get employee method

--- src/main/java/com/mockUIBackend/service/EmployeeService.java
+++ src/main/java/com/mockUIBackend/service/EmployeeService.java
@@ -1,10 +1,17 @@
 package com.mockUIBackend.service;
 
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mockUIBackend.DTO.CityDTO;
+import com.mockUIBackend.DTO.DesignationDTO;
+import com.mockUIBackend.DTO.EmployeeDTO;
+import com.mockUIBackend.exception.EmplNotFoundException;
+import com.mockUIBackend.model.Employee;
 import com.mockUIBackend.repository.CityRepo;
 import com.mockUIBackend.repository.DesignationRepo;
 import com.mockUIBackend.repository.EmployeeRepo;
@@ -21,6 +28,52 @@ public class EmployeeService {
 	
 	@Autowired
 	private DesignationRepo desigRepo;
+	
+	
+	public EmployeeDTO getEmployee (Integer id) throws EmplNotFoundException {
+		
+		Optional<Employee> optionalEmployee = empRepo.findById(id);
+		Employee employee = optionalEmployee.orElseThrow(() -> 
+			new EmplNotFoundException("Employee Not Found"));
+		
+		EmployeeDTO eDTO = EmployeeDTO.prepareDTO(employee);
+		DesignationDTO dDTO = DesignationDTO.prepareDTO(employee.getDesignation());
+		CityDTO cDTO = CityDTO.prepareDTO(employee.getCity());
+		
+		eDTO.setDesignation(dDTO);
+		eDTO.setCity(cDTO);
+		
+		return eDTO;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
