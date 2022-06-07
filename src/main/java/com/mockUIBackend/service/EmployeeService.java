7dee849052e80a99b314703f0d94ce0ef855267f
kevinjoseph97 <kevinjoseph97@gmail.com>
2022-06-07 16:46:55 -0400

    fixed errors

--- src/main/java/com/mockUIBackend/service/EmployeeService.java
+++ src/main/java/com/mockUIBackend/service/EmployeeService.java
@@ -1,89 +1,24 @@
 package com.mockUIBackend.service;
 
-import java.util.Optional;
+import java.util.List;
 
-import javax.transaction.Transactional;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.mockUIBackend.DTO.CityDTO;
-import com.mockUIBackend.DTO.DesignationDTO;
 import com.mockUIBackend.DTO.EmployeeDTO;
 import com.mockUIBackend.exception.EmplNotFoundException;
-import com.mockUIBackend.model.Employee;
-import com.mockUIBackend.repository.CityRepo;
-import com.mockUIBackend.repository.DesignationRepo;
-import com.mockUIBackend.repository.EmployeeRepo;
 
-@Transactional
-@Service(value = "emplServices")
-public class EmployeeService {
+public interface EmployeeService {
 	
-	@Autowired
-	private EmployeeRepo empRepo;
+	public EmployeeDTO getEmployee(Integer id);
 	
-	@Autowired
-	private CityRepo cityRepo;
+	public List<EmployeeDTO> getAllEmployees();
 	
-	@Autowired
-	private DesignationRepo desigRepo;
+	public Integer addEmployee(EmployeeDTO employeeDTO);
+	
+	public void updateEmployee(Integer id, EmployeeDTO employeeDTO);
 	
 	
+	List<String> getDesignations() throws EmplNotFoundException;
 	
-	public EmployeeDTO getEmployee (Integer id) throws EmplNotFoundException {
-		
-		Optional<Employee> optionalEmployee = empRepo.findById(id);
-		Employee employee = optionalEmployee.orElseThrow(() -> 
-			new EmplNotFoundException("Employee Not Found"));
-		
-		EmployeeDTO eDTO = EmployeeDTO.prepareDTO(employee);
-		DesignationDTO dDTO = DesignationDTO.prepareDTO(employee.getDesignation());
-		CityDTO cDTO = CityDTO.prepareDTO(employee.getCity());
-		
-		eDTO.setDesignation(dDTO);
-		eDTO.setCity(cDTO);
-		
-		return eDTO;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	List<String> getCities() throws EmplNotFoundException;
 	
 	
 
--- src/main/java/com/mockUIBackend/service/EmployeeServiceImpl.java
+++ src/main/java/com/mockUIBackend/service/EmployeeServiceImpl.java
@@ -1,25 +1,129 @@
 package com.mockUIBackend.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mockUIBackend.DTO.CityDTO;
+import com.mockUIBackend.DTO.DesignationDTO;
 import com.mockUIBackend.DTO.EmployeeDTO;
 import com.mockUIBackend.exception.EmplNotFoundException;
+import com.mockUIBackend.model.Employee;
+import com.mockUIBackend.repository.CityRepo;
+import com.mockUIBackend.repository.DesignationRepo;
+import com.mockUIBackend.repository.EmployeeRepo;
 
-public interface EmployeeServiceImpl {
+@Transactional
+@Service(value = "emplServices")
+public class EmployeeServiceImpl implements EmployeeService {
 	
-	public EmployeeDTO getEmployee(Integer id);
+	@Autowired
+	private EmployeeRepo empRepo;
 	
-	public List<EmployeeDTO> getAllEmployees();
+	@Autowired
+	private CityRepo cityRepo;
 	
-	public Integer addEmployee(EmployeeDTO employeeDTO);
-	
-	public void updateEmployee(Integer id, EmployeeDTO employeeDTO);
-	
-	
-	List<String> getDesignations() throws EmplNotFoundException;
-	
-	List<String> getCities() throws EmplNotFoundException;
+	@Autowired
+	private DesignationRepo desigRepo;
 	
 	
+	@Override
+	public EmployeeDTO getEmployee (Integer id) throws EmplNotFoundException {
+		
+		Optional<Employee> optionalEmployee = empRepo.findById(id);
+		Employee employee = optionalEmployee.orElseThrow(() -> 
+			new EmplNotFoundException("Employee Not Found"));
+		
+		EmployeeDTO eDTO = EmployeeDTO.prepareDTO(employee);
+		DesignationDTO dDTO = DesignationDTO.prepareDTO(employee.getDesignation());
+		CityDTO cDTO = CityDTO.prepareDTO(employee.getCity());
+		
+		eDTO.setDesignation(dDTO);
+		eDTO.setCity(cDTO);
+		
+		return eDTO;
+	}
 
+
+	@Override
+	public List<EmployeeDTO> getAllEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Integer addEmployee(EmployeeDTO employeeDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void updateEmployee(Integer id, EmployeeDTO employeeDTO) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public List<String> getDesignations() throws EmplNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<String> getCities() throws EmplNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
